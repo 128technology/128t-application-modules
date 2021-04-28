50492dda3d6e53d62d58d543cdd1692ac42148d3
Patrick Timmons <ptimmons@gmail.com>
2021-04-28 19:51:04 -0400

    Resolves #12 -- reverted back to ASN-format (#13)
    Co-authored-by: ptimmons <ptimmons@128technology.com>

--- facebook/facebook.py
+++ facebook/facebook.py
@@ -1,20 +1,50 @@
 #!/usr/bin/python
 ###############################################################################
-# Facebook application module
-# 28-Apr-2021, Patrick Timmons (with contributions by Joe Developer)
+# Facebook application module (ASN-style)
+# 28-April-2021, Patrick Timmons
 ###############################################################################
 
 import json
-import os
-import re
 import sys
 import urllib2
+import ipaddress
 
-BASE_PATH = '/etc/128technology/application-modules'
-sys.path.append(BASE_PATH)
+sys.path.append('/etc/128technology/application-modules')
 import app_module_utils
 
-URL = 'https://www.netify.ai/resources/applications/facebook'
+ASN = ['AS32934', 'AS63293']
+URL = 'https://stat.ripe.net/data/announced-prefixes/data.json?resource='
+
+"""
+RIPE is kind enough to provide access to all prefixes announced per ASN, and this
+is the resource we're using here. The HTTPS query above will return JSON-formatted
+output like the following:
+
+--
+{
+    "status": "ok",
+    "server_id": "app021",
+    "status_code": 200,
+    "version": "1.2",
+    ...
+    "data": {
+        "resource": "13445",
+        "prefixes": [
+            {
+                "timelines": [
+                    {
+                        "endtime": "2020-06-10T08:00:00",
+                        "starttime": "2020-05-27T08:00:00"
+                    }
+                ],
+                "prefix": "208.253.254.0/24"
+            },
+     ...
+--
+The python script then will then collect each 'prefix' and create the appID-
+formatted JSON output as /var/run/128technology/application-module/webex.json
+
+"""
 
 MODULE_NAME = 'facebook'
 SERVICE_NAME = 'FACEBOOK'
@@ -22,19 +52,16 @@ SERVICE_NAME = 'FACEBOOK'
 def main():
     app_id = app_module_utils.AppIdBuilder(MODULE_NAME, 3600)
 
-    response = urllib2.urlopen(URL)
-    lines = response.readlines()
-
-    for line in lines:
-        matches = re.search("^\s*<li>(([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2])))?<\/li>", line)
-        if matches is not None:
-            cidr = matches.group(1)
-
-            app_id.add_entry(SERVICE_NAME, cidr)
-            app_id.error = ''
-#        else:
-#            app_id.error = 'No mappings found. Perhaps you are requesting too quickly?'
-
+    for asn in ASN:
+        url = URL + asn
+        response = urllib2.urlopen(url)
+        jResponse = json.loads(response.read())
+        for prefixes in jResponse["data"]["prefixes"]:
+            try:
+                v4prefix = ipaddress.IPv4Network(prefixes["prefix"])
+                app_id.add_entry(SERVICE_NAME, str(v4prefix))
+            except:
+                continue
     app_id.write_to_disk()
 
 if __name__ == '__main__':
