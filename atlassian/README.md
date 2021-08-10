1b7806090bf09240ab9bc8a8e336139aa29c1c03
ptimmons <ptimmons@128technology.com>
2021-08-10 13:10:21 -0400

    Initial commit: Atlassian

---
+++ atlassian/README.md
@@ -0,0 +1,10 @@
+# 128T AppID Module: Atlassian
+
+This AppID module will identify traffic associated with Atlassian's products (Jira, Confluence, etc.); adding this module to your system will identify the new application-name "ATLASSIAN" which can then be prioritized using service-policy, service-route handling, etc.
+
+> Note: the script operates by retrieving a publicly available JSON document, published by Atlassian for this purpose.
+
+The service name for this module is **ATLASSIAN**. Please reference this in the `application-name` field of the service you create.
+
+Disclaimer: this is a community contributed module.
+
---
+++ atlassian/atlassian.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+###############################################################################
+# Zendesk application module
+# 29-July-2021, Patrick Timmons
+###############################################################################
+
+import json
+import sys
+import urllib3
+import ipaddress
+
+sys.path.append('/etc/128technology/application-modules')
+import app_module_utils
+
+URL = 'https://ip-ranges.atlassian.com/'
+
+"""
+Atlassian conveniently offers a public API that lets you retrieve the list of
+IP addresses that they use for ingress traffic and egress traffic.
+
+More information at the following links:
+https://support.atlassian.com/organization-administration/docs/ip-addresses-and-domains-for-atlassian-cloud-products/
+
+"""
+
+MODULE_NAME = 'atlassian'
+SERVICE_NAME = 'ATLASSIAN'
+
+def main():
+    app_id = app_module_utils.AppIdBuilder(MODULE_NAME, 3600)
+
+    http = urllib3.PoolManager()
+    response = http.request('GET', URL)
+    if response.status == 200:
+        jResponse = json.loads(response.data.decode('utf-8'))
+        for prefix in jResponse["items"]["cidr"]:
+            try:
+                v4prefix = ipaddress.IPv4Network(prefix)
+                app_id.add_entry(SERVICE_NAME, str(v4prefix))
+            except:
+                continue
+    app_id.write_to_disk()
+
+if __name__ == '__main__':
+    main()
