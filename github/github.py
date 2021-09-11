d2debd165b97fd3a94bde36a8202dc1696e59f8d
ptimmons <ptimmons@128technology.com>
2021-09-11 17:07:44 -0400

    WIP: adding github

--- github/github.py
+++ github/github.py
@@ -29,6 +29,7 @@ def main():
     app_id = app_module_utils.AppIdBuilder(MODULE_NAME, 86400)
 
     user_agent = {'User-Agent': 'SSR Github AppID Module; github.com/128technology/128t-application-modules'}
+    http = urllib3.PoolManager()
     response = http.request('GET', URL, headers=user_agent)
     print(response.data.decode('utf-8'))
     if response.status == 200:
